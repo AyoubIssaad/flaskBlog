11b798087dbd87e20660c18f007d8fa6fba50345
ayoubissaad <ayoub.issaad@gmail.com>
2021-07-16 18:01:56 +0200

    tr

--- flaskblog.py
+++ flaskblog.py
@@ -2,5 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/home")
+def home():
+    """Take me to route"""
+    return "<h1>Hello, World!!</h1>"
+
+
+@app.route("/about")
+def about():
+    """Take me to route"""
+    return "<h1>About page</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)

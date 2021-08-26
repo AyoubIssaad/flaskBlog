66103a39365deb95bcb5ac70b1443e655843d525
Ayoub Issaad <ayoub.issaad@gmail.com>
2021-08-26 22:32:09 +0100

    first two videos

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Ayoub ISSAAD',
+        'title':'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20,2018'
+    },
+    {
+        'author':'Core Schafer',
+        'title':'Blog Post 2',
+        'content': 'second post',
+        'date_posted': 'April 21,2018'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
     """Take me to route"""
-    return "<h1>Hello, World!!</h1>"
+    return render_template('home.html', posts=posts, title='Home')
 
 
 @app.route("/about")
 def about():
     """Take me to route"""
-    return "<h1>About page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
